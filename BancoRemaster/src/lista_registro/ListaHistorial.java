24a180bb9a4c7aef2ef2eebd9766cb3a94242ce8
Endjrx <endijosueparodi66@gmail.com>
2024-07-25 20:46:46 -0500

    Solucion al contar elementos y agregarlos al array

--- BancoRemaster/src/lista_registro/ListaHistorial.java
+++ BancoRemaster/src/lista_registro/ListaHistorial.java
@@ -30,22 +30,52 @@ public class ListaHistorial {
     }
 
 
-    public void obtenerDatos () {
+    public NodoHistorial [] obtenerDatos () {
 
         if (inicio == null) {
-            return;
+            return null;
+        } else {
+
+            int cont = 0;
+
+            NodoHistorial [] historiales = new NodoHistorial[obtenerTamaño()];
+            NodoHistorial aux = inicio;
+
+            while (true) {
+
+                historiales [cont] = aux;
+                cont++;
+                
+                if (aux.getSiguiente() == null) break;
+                aux = aux.getSiguiente();
+
+            }
+
+            return historiales;
+        }
+
+    }
+
+    public int obtenerTamaño () {
+
+        int contador = 1;
+
+        if (inicio == null) {
+            return 0;
         } else {
 
             NodoHistorial aux = inicio;
             while (aux.getSiguiente() != null) {
-                
-                System.out.println("Saldo: " + aux.getHistorial().getSaldo());
+            
                 aux = aux.getSiguiente();
-                
+                contador++;
+
             }
 
+            return contador;
         }
 
     }
+    
 
 }
